e08467eec243f3ca229bfbd03b97b1064392cf61
manonaSamir <bena.yalla@yahoo.com>
2024-04-29 20:31:51 +0300

    return aminity

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Definition of class Amenity."""
-    from models import storage_t
-    __tablename__ = "amenities"
-    __table_args__ = (
-        {'mysql_default_charset': 'latin1'})
-    if storage_t == "db":
-        from models.place import place_amenity
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary=place_amenity,
-                                       back_populates="amenities")
     else:
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
