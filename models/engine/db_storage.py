6589ba79b6b11287305349b9a1bea468c0483811
manonaSamir <bena.yalla@yahoo.com>
2024-04-29 20:25:28 +0300

    fix storage model

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,17 +71,35 @@ class DBStorage:
         Session = scoped_session(sess_factory)
         self.__session = Session
 
-    def get(self, cls, id):
-        """Retreives an object by class name and id."""
-        key = cls.__name__ + '.' + id
-        return self.all(cls).get(key)
-
-    def count(self, cls=None):
-        """Returns the number of objects in storage."""
-        if not cls:
-            return len(self.all())
-        return len(self.all(cls))
-
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class name and its ID, or
+        None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return value
+
+        return None
+
+    def count(self, cls=None):
+        """
+        count the number of objects in storage
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+        return count
