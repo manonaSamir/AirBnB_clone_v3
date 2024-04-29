2ccce85261e9d2b37ad1964de164871db2fd36f8
manonaSamir <bena.yalla@yahoo.com>
2024-04-29 20:30:48 +0300

    change amity table

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,20 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
+#!/usr/bin/python3
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
+    """Definition of class Amenity."""
+    from models import storage_t
+    __tablename__ = "amenities"
+    __table_args__ = (
+        {'mysql_default_charset': 'latin1'})
+    if storage_t == "db":
+        from models.place import place_amenity
         name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
     else:
         name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
