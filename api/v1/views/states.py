d11f42eb09a4232d8f817c6e7762bd842835f164
manonaSamir <bena.yalla@yahoo.com>
2024-05-01 20:15:57 +0300

    sandbox

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -50,8 +50,9 @@ def post_state():
     if not request.get_json():
         abort(400, description='Not a JSON')
     data = request.get_json()
-    if 'name' not in data or not data['name']:
+    if 'name' not in request.get_json() or not data['name']:
         abort(400, description='Missing or empty name')
+
     instance = State(**data)
     instance.save()
     return make_response(jsonify(instance.to_dict()), 201)
